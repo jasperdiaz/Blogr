bff003b6efe3d221bac07bab4c49527333810259
Jasper Diaz <jasperdiaz45@gmail.com>
2021-05-10 14:56:52 +0800

    Features

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body');
-const feature = document.querySelector('.feature');
+const hero = document.querySelector('.hero');
 const navButton = document.querySelector('#navigation-toggle');
 const hamburgerMenu = document.querySelector('#mobile-menu');
 
@@ -9,20 +9,21 @@ function navMenuToggle() {
     if(hamburgerMenu.classList.contains('hidden')) {
         hamburgerMenu.classList.remove('hidden');
         hamburgerMenu.classList.add('show');
-        feature.classList.remove('flex');
-        feature.classList.add('hidden');
+        hero.classList.remove('flex');
+        hero.classList.add('hidden');
         // body.classList.add('scroll-hidden');
     }else{
         hamburgerMenu.classList.remove('show');
         hamburgerMenu.classList.add('hidden');
-        feature.classList.remove('hidden');
-        feature.classList.add('flex');
+        hero.classList.remove('hidden');
+        hero.classList.add('flex');
         // body.classList.remove('scroll-hidden');
     }
 }
 
 function accordionToggle() {
     document.querySelector('#' + this.id).nextElementSibling.classList.toggle('flex');
+    document.querySelector('#' + this.id).nextElementSibling.classList.toggle('hidden');
 }
 
 navButton.addEventListener('click', navMenuToggle);
